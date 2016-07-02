ade852b25fc7c8ed54620c5521aabb2438ede5d5
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-07-02 14:48:28 +0200

    Phylogeny workflow added.

--- recipes/data.rb
+++ recipes/data.rb
@@ -2,4 +2,37 @@
 # Cookbook Name:: phylogeny
 # Recipe:: data
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright (c) 2016 Jörgen Brandt, All Rights Reserved.
+
+
+fa_dir = "#{node.dir.data}/fa"
+seed_dir = "#{node.dir.data}/seed"
+
+chase_seed = "#{seed_dir}/PF03924_seed.txt"
+
+link_lst = [
+            "ftp://ftp.jgi-psf.org/pub/JGI_data/Chlamy/v4.0/annotation/Chlre4_all_proteins.fasta.gz",
+            "ftp://ftp.jgi-psf.org/pub/JGI_data/Physcomitrella_patens/v1.1/proteins.Phypa1_1.AllModels.fasta.gz",
+            "ftp://ftp.jgi-psf.org/pub/JGI_data/Aureococcus_anophageferrens/annotation/v1.0/proteins.Auran1_FilteredModels3.fasta.gz",
+            "ftp://ftp.jgi-psf.org/pub/JGI_data/Chlorella_NC64A/annotation/v1.0/Chlorella_NC64A.all_proteins.fasta.gz",
+            "ftp://ftp.jgi-psf.org/pub/JGI_data/Chlorella_vulgaris/annotation/v1.0/Chlvu1_all_proteins_3.fasta.gz",
+            "ftp://ftp.jgi-psf.org/pub/JGI_data/Ehuxleyi/annotation/v1.0/Emihu1_all_proteins.fasta.gz",
+            "ftp://ftp.jgi-psf.org/pub/JGI_data/Fragilariopsis_cylindrus/v1.0/Fracy1_GeneModels_FilteredModels2_aa.fasta.gz"
+           ]
+
+directory node.dir.data
+directory fa_dir
+directory seed_dir
+
+link_lst.each { |link|
+
+  remote_file "#{fa_dir}/#{File.basename( link )}" do
+    action :create_if_missing
+    source link
+    retries 1
+  end
+}
+
+cookbook_file chase_seed do
+  source "PF03924_seed.txt"
+end
--- recipes/tools.rb
+++ recipes/tools.rb
@@ -3,3 +3,10 @@
 # Recipe:: tools
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe "chef-bioinf-worker::hmmer"
+include_recipe "chef-bioinf-worker::muscle"
+include_recipe "chef-bioinf-worker::clustalw"
+include_recipe "chef-bioinf-worker::phylip"
+
+package "ghostscript"
